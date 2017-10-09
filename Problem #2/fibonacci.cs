5321d54fc6ac05a892e2ab6250a6b152863f9bf6
Lucas Siqueira <luc.siqueira94@gmail.com>
2017-10-08 22:38:32 -0300

    Edição da identação

--- Problem #2/fibonacci.cs
+++ Problem #2/fibonacci.cs
@@ -2,20 +2,19 @@
     class MainClass
     {
         public static void Main(string[] args)
-        {
-			int count = 0;
-            int aux = 0;
-			int fib1 = 1;
-			int fib2 = 2;
-			while (fib1 <= 4000000)
-			{
-				if (fib1 % 2 == 0)
-					count += x;
-                aux = fib1 + fib2;
-                fib1 = fib2;
-                fib2 = aux;
-			}
-            Console.WriteLine(count);
+	{
+		int count = 0;
+		int aux = 0;
+		int fib1 = 1;
+		int fib2 = 2;
+		while (fib1 <= 4000000)
+		{
+			if (fib1 % 2 == 0) count += x;
+                	aux = fib1 + fib2;
+                	fib1 = fib2;
+                	fib2 = aux;
+		}
+            	Console.WriteLine(count);
         }
     }
 }
